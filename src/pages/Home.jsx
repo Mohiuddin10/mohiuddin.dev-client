4eb6009e95512e43f7bd6059d04ac91eda767b13
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-05-30 23:49:22 +0600

    hero text move

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,12 @@ const Home = () => {
       className="hero min-h-screen"
       style={{
         backgroundImage: `url(${bgImage})`,
+        position: "relative",
+        backgroundSize: "cover",
       }}
     >
       <div className="hero-overlay"></div>
-      <div className="hero-content text-neutral-content text-left">
+      <div className="hero-content flex-row justify-start items-start text-neutral-content border-2 p-4">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
           <p className="mb-5">
